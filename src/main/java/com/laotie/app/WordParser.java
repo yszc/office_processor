b52ffe7a2098e110e93d1c4fcd32dfd1bf51b7ad
laotie <chenzhijun@fongwell.com>
2024-01-10 03:06:38 +0000

    改为非静态

--- src/main/java/com/laotie/app/WordParser.java
+++ src/main/java/com/laotie/app/WordParser.java
@@ -3,7 +3,8 @@ package com.laotie.app;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xwpf.usermodel.*;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.FileInputStream;
@@ -15,73 +16,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordParser {
+    private XWPFDocument document;
+    private XWPFStyles style_sheet;
+    private Section nestedRoot;
 
     public static void main(String[] args) {
         String filePath = "/workspaces/wordparser/docs/template.docx";
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            XWPFDocument document = new XWPFDocument(fis);
+        try {
+            WordParser wordParser = new WordParser(filePath);
+            Section root = wordParser.parseTemplate();
 
-            Section root = parseDocument(document);
-            root = filterSection(root);
-
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root.getChildren());
+            String jsonResult = root.toNoneEmpty().toJson();
             System.out.println(jsonResult);
 
-            List<Section> sections = mapper.readValue(jsonResult, new TypeReference<List<Section>>() { });
-            Section rootBack = new Section();
-            rootBack.setChildren(sections);
+            Section rootBack = Section.fromJson(jsonResult);
             System.out.println(rootBack);
+        } catch (Exception e) {
 
+        }
+    }
+
+    public WordParser(String filePath) throws IOException {
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            this.document = new XWPFDocument(fis);
+            // 样式表
+            this.style_sheet = document.getStyles();
+            this.nestedRoot = new Section("root", "root");
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
-    }
-
-    /**
-     * 过滤所有空标题
-     * 
-     * @param root
-     * @return
-     */
-    private static Section filterSection(Section root) {
-        if (null == root.getChildren()) {
-            return root;
-        }
-        for (Section child : root.getChildren()) {
-            filterSection(child);
-        }
-        root.setChildren(new ArrayList<>(CollectionUtils.select(root.getChildren(),
-                child -> null != child && (child.getType() == "input" || child.getChildren().size() > 0))));
-        return root;
-    }
-
-    /**
-     * 获得标题的层级
-     * 
-     * @param heading
-     * @return
-     */
-    private static int getHeadingIndent(String heading) {
-        if (heading.startsWith("heading")) {
-            return Integer.parseInt(heading.replace("heading", "").trim());
-        }
-        return 0;
-    }
-
-    /**
-     * 创建一个子节点
-     * 
-     * @param type
-     * @param name
-     * @return
-     */
-    private static Section createSection(String type, String name) {
-        Section paraSection = new Section();
-        paraSection.setType(type);
-        paraSection.setName(name);
-        paraSection.setChildren(new ArrayList<>());
-        return paraSection;
     }
 
     /**
@@ -90,63 +54,90 @@ public class WordParser {
      * @param document
      * @return
      */
-    private static Section parseDocument(XWPFDocument document) {
+    public Section parseTemplate() {
         Stack<Section> stack = new Stack<>();
-        Section root = createSection("root", "root");
-        stack.push(root);
-
-        // 样式表
-        XWPFStyles style_sheet = document.getStyles();
+        stack.push(nestedRoot);
 
         for (IBodyElement element : document.getBodyElements()) {
             if (element instanceof XWPFParagraph) {
-                // 解析heading和段落占位符
                 XWPFParagraph paragraph = (XWPFParagraph) element;
-                String paraText = paragraph.getText();
-                String styleID = paragraph.getStyleID();
-                String styleName = "";
-                if (styleID != null) {
-                    styleName = style_sheet.getStyle(styleID).getName().toLowerCase();
-                }
-                if (styleName.startsWith("heading")) {
-                    int indent = getHeadingIndent(styleName);
-                    if (indent <= 0) {
-                        continue;
-                    }
-                    while (stack.size() > indent) {
-                        stack.pop();
-                    }
-                    Section paraSection = createSection("title", paraText);
-                    if (!stack.isEmpty()) {
-                        stack.peek().getChildren().add(paraSection);
-                    }
-                    String parentPrefix = stack.peek().getPrefix();
-                    List<Section> brothers = stack.peek().getChildren();
-                    if (parentPrefix.length() > 0) {
-                        paraSection.setPrefix(parentPrefix + "." + brothers.size());
-                    } else {
-                        paraSection.setPrefix(String.valueOf(brothers.size()));
-                    }
-                    stack.push(paraSection);
-                } else {
-                    // 查找占位符
-                    for (Section input : parseInput(paraText)) {
-                        stack.peek().getChildren().add(input);
-                    }
-                }
+                parseParagraph(paragraph, stack);
             } else if (element instanceof XWPFTable) {
-                // 查找table中的占位符
                 XWPFTable table = (XWPFTable) element;
-                for (XWPFTableRow row : table.getRows()) {
-                    for (XWPFTableCell cell : row.getTableCells()) {
-                        for (Section input : parseInput(cell.getText())) {
-                            stack.peek().getChildren().add(input);
-                        }
-                    }
+                parseTable(table, stack);
+            }
+        }
+        return nestedRoot;
+    }
+
+    /**
+     * 解析table，从每个单元格中解析占位符。
+     * 
+     * @param table
+     * @param stack
+     */
+    private void parseTable(XWPFTable table, Stack<Section> stack) {
+        for (XWPFTableRow row : table.getRows()) {
+            for (XWPFTableCell cell : row.getTableCells()) {
+                for (Section input : parseInput(cell.getText())) {
+                    stack.peek().getChildren().add(input);
                 }
             }
         }
-        return root;
+    }
+
+    /**
+     * 解析段落信息，包括heading和普通段落
+     * 
+     * @param paragraph
+     * @param stack
+     */
+    private void parseParagraph(XWPFParagraph paragraph, Stack<Section> stack) {
+        String paraText = paragraph.getText();
+        String styleID = paragraph.getStyleID();
+        String styleName = "";
+        if (styleID != null) {
+            styleName = style_sheet.getStyle(styleID).getName().toLowerCase();
+        }
+        if (styleName.startsWith("heading")) {
+            int indent = parseHeadingIndent(styleName);
+            if (indent <= 0) {
+                return;
+            }
+            while (stack.size() > indent) {
+                stack.pop();
+            }
+            Section paraSection = new Section("title", paraText);
+            if (!stack.isEmpty()) {
+                stack.peek().getChildren().add(paraSection);
+            }
+            String parentPrefix = stack.peek().getPrefix();
+            List<Section> brothers = stack.peek().getChildren();
+            if (parentPrefix.length() > 0) {
+                paraSection.setPrefix(parentPrefix + "." + brothers.size());
+            } else {
+                paraSection.setPrefix(String.valueOf(brothers.size()));
+            }
+            stack.push(paraSection);
+        } else {
+            // 查找占位符
+            for (Section input : parseInput(paraText)) {
+                stack.peek().getChildren().add(input);
+            }
+        }
+    }
+
+    /**
+     * 获得标题的层级
+     * 
+     * @param heading
+     * @return
+     */
+    private static int parseHeadingIndent(String heading) {
+        if (heading.startsWith("heading")) {
+            return Integer.parseInt(heading.replace("heading", "").trim());
+        }
+        return 0;
     }
 
     // 解析输入占位符
@@ -158,7 +149,7 @@ public class WordParser {
         List<Section> result = new ArrayList<>();
         while (matcher.find()) {
             String match = matcher.group(0); // 获取第一个捕获组的内容
-            Section paraSection = createSection("input", match);
+            Section paraSection = new Section("input", match);
             result.add(paraSection);
         }
         return result;
@@ -169,6 +160,56 @@ public class WordParser {
         private String prefix = "";
         private String name;
         private List<Section> children;
+
+        /**
+         * 创建一个子节点
+         * 
+         * @param type
+         * @param name
+         * @return
+         */
+        public Section(String type, String name) {
+            this.type = type;
+            this.name = name;
+            this.children = new ArrayList<>();
+        }
+
+        public String toJson() throws JsonProcessingException {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        }
+
+        public static Section fromJson(String jsonResult) throws JsonMappingException, JsonProcessingException {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(jsonResult, Section.class);
+        }
+
+        /**
+         * 获取非空的结构
+         * 
+         * @return
+         */
+        public Section toNoneEmpty() {
+            return filterSection(this);
+        }
+
+        /**
+         * 过滤所有空标题
+         * 
+         * @param root
+         * @return
+         */
+        private static Section filterSection(Section root) {
+            if (null == root.getChildren()) {
+                return root;
+            }
+            for (Section child : root.getChildren()) {
+                filterSection(child);
+            }
+            root.setChildren(new ArrayList<>(CollectionUtils.select(root.getChildren(),
+                    child -> null != child && (child.getType() == "input" || child.getChildren().size() > 0))));
+            return root;
+        }
 
         public String getType() {
             return type;

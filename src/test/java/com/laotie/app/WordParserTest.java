d21537124f624ac29d9549ff7fddbb2f7617b67c
Zhijun <chenzhijun@fongwell.com>
2024-04-24 11:15:19 +0800

    clear error

--- src/test/java/com/laotie/app/WordParserTest.java
+++ src/test/java/com/laotie/app/WordParserTest.java
@@ -1,18 +1,30 @@
 package com.laotie.app;
 import java.io.IOException;
-
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+
 class WordParserTest {
+    String filePath = "docs/template.docx";
 
     @Test
     void testParseTemplate() throws IOException {
-        String filePath = "docs/jsoncase.docx";
         WordParser wordParser = new WordParser(filePath);
         Section root = wordParser.parseTemplate();
 
         String jsonResult = root.toFormFriendly().toNoneEmpty().toJson();
         System.out.println(jsonResult);
     }
+
+    @Test
+    void testFormList() throws IOException {
+        WordParser wordParser = new WordParser(filePath);
+        Section root = wordParser.parseTemplate();
+        List<JSONObject> formList = root.fetchAllInputAttr();
+        System.out.println(new JSONArray(formList));
+    }
+
 
 }
